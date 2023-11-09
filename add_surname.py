5921a19605b42b35f5e973c8251a7e54df2b3233
Keddins <eddinskeathan@gmail.com>
2023-11-08 20:44:28 -0800

    surname

--- add_surname.py
+++ add_surname.py
@@ -1,7 +1,6 @@
 def add_surname(first_names: list) -> list:
     return [name + " Kardashian" for name in first_names if name.startswith("K")]
 
-# Example usage
 if __name__ == "__main__":
     original_names = ["Kiki", "Krystal", "Pavel", "MaryKay", "Annie", "Koala"]
     modified_names = add_surname(original_names)
